9c9addc4ab0e31ccdc41e11d0f47bd887b9e817f
David <weisdorf.david@gmail.com>
2018-11-13 15:45:19 -0500

    pwm freq change code

--- demo_code/demo_code.ino
+++ demo_code/demo_code.ino
@@ -5,12 +5,12 @@
 // PINS
 
 // motor 1 = motor right : pins
-int ENC1_PIN_A = 18, ENC1_PIN_B = 19;
-int MOT1_H = 7, MOT1_L = 6;
+int ENC1_PIN_A = 19, ENC1_PIN_B = 18;
+int MOT1_H = 7, MOT1_L = 8;
 
 // motor 2 = motor left : pins
 int ENC2_PIN_A = 2, ENC2_PIN_B = 3;
-int MOT2_H = 10, MOT2_L = 11;
+int MOT2_H = 5, MOT2_L = 6;
 
 // button pin
 int BUT_SET = 13, BUT_MES = 12;
@@ -22,33 +22,46 @@ Motor mot2(MOT2_H, MOT2_L, ENC2_PIN_A, ENC2_PIN_B);
 void updateEnc1() { mot1.updateEncoder(); }
 void updateEnc2() { mot2.updateEncoder(); }
 
+
 void setup() 
 {
   // initialize motor 1
-  mot1.initialize();
-  attachInterrupt(digitalPinToInterrupt(ENC1_PIN_A), updateEnc1, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(ENC1_PIN_B), updateEnc1, CHANGE);
+//  mot1.initialize();
+//  attachInterrupt(digitalPinToInterrupt(ENC1_PIN_A), updateEnc1, CHANGE);
+//  attachInterrupt(digitalPinToInterrupt(ENC1_PIN_B), updateEnc1, CHANGE);
   
   // initialize motor 2
-  mot2.initialize();
-  attachInterrupt(digitalPinToInterrupt(ENC2_PIN_A), updateEnc2, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(ENC2_PIN_B), updateEnc2, CHANGE);
+//  mot2.initialize();
+//  attachInterrupt(digitalPinToInterrupt(ENC2_PIN_A), updateEnc2, CHANGE);
+//  attachInterrupt(digitalPinToInterrupt(ENC2_PIN_B), updateEnc2, CHANGE);
+
+  // change up PWM frequencies for motor PWM pins
+  // Timer 3 for outputs 2/3/5
+  TCCR3B &= ~(0x07);
+  TCCR3B |= 0x02;
   
-  // initialize serial
+  // Timer 4 for outputs 6,7,8  
+  TCCR4B &= ~(0x07);
+  TCCR4B |= 0x02;
+  
+// initialize serial
 //  Serial.begin (9600);
 
   // initialize button 
   pinMode(BUT_SET, OUTPUT);
   digitalWrite(BUT_SET, 1);
   pinMode(BUT_MES, INPUT);
-  
+
+
 }
 
 int state = 0;
 int stateCounter = 0;
+unsigned endTime;
 
 void loop()
 {
+   
   const int PERIOD = 1;
   delay(PERIOD);
 
@@ -56,56 +69,47 @@ void loop()
   if(!state && (digitalRead(BUT_MES) == LOW)) return;
   if(!state) state = 1;
 
-  bool testM1 = true;
-  bool testForward = true;
-  int signV = testForward ? 1 : -1;
-
   if(state == 1)
   { 
     state = 2;
-
-    digitalWrite(MOT1_H, HIGH);
-    digitalWrite(MOT1_L, LOW);
-    digitalWrite(MOT2_H, HIGH);
-    digitalWrite(MOT2_L, LOW);
+    
+//    mot1.setGains(0, 0.00000001);
+//    mot2.setGains(0, 0.00000001);
     
     stateCounter = 0;
   }
   else if(state == 2)
   { 
-    mot1.setSetpoint(20);
-    mot1.setGains(60, 2);
+      stateCounter++;
+      int pwm = stateCounter * 255.0 / 5000.0;
 
-    mot2.setSetpoint(20);
-    mot2.setGains(60, 2);
-
-    stateCounter++;
-    if(stateCounter > 5000)
-    {
-      state = 999;
-
-      analogWrite(MOT1_H, 0);
-      analogWrite(MOT1_L, 0);
-      analogWrite(MOT2_H, 0);
-      analogWrite(MOT2_L, 0);
-      
-      mot2.setGains(0, 0);
-      mot1.setGains(0, 0);
-      mot1.setSetpoint(0);
-      mot2.setSetpoint(0);
-    }
-  }
-
-  // motor controls
-  if(state < 10)
-  {
-      static int updateCounter = 0;
-      if(++updateCounter > 10)
+      if(pwm < 256)
       {
-//          mot1.update(PERIOD);
-//          mot2.update(PERIOD);        
-          updateCounter = 0;
+        analogWrite(MOT1_L, pwm);
+        digitalWrite(MOT1_H, LOW);    
+        analogWrite(MOT2_L, pwm);
+        digitalWrite(MOT2_H, LOW);
+  
       }
+      else
+      {   
+        state = 3;
+        stateCounter = 0;
+      }    
+
   }
+  else if(state == 3)
+  {
+    if(++stateCounter > 5000)
+      state = 999;
+  }
+  else
+  {
+    digitalWrite(MOT1_H, LOW);
+    digitalWrite(MOT1_L, LOW);    
+    digitalWrite(MOT2_H, LOW);
+    digitalWrite(MOT2_L, LOW);
+  }
+
   
 }
