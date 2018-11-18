7c2fb454c97616239e54e3afd9817cce9cac6b93
Alessandra KS <alessandraschlums@gmail.com>
2018-11-17 20:57:32 -0500

    initial commit

--- course_code/nav.h
+++ course_code/nav.h
@@ -3,6 +3,19 @@
 
 #define MOTOR_V_MAX 255
 
+int IN1=4;
+int IN2=5;
+int ENA=10;
+
+int IN3=6;
+int IN4=7;
+int ENA2=11;
+
+pinMode(IN1,OUTPUT);
+pinMode(IN2,OUTPUT); 
+pinMode(IN3,OUTPUT); 
+pinMode(IN4,OUTPUT); 
+
 struct MotorCommand
 {
 	int leftV;
