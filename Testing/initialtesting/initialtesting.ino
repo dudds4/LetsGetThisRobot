8dacf6195a2612f0223b447960a352790088f8e8
David <weisdorf.david@gmail.com>
2018-11-21 12:44:56 -0500

    removed test stuff

--- Testing/initialtesting/initialtesting.ino
+++ Testing/initialtesting/initialtesting.ino
@@ -11,37 +11,19 @@ void setup()
 {
   Serial.begin(9600);
   
-//  initializeIMU(bno);
-//  initializeIR();
-//  initMotors();
-  
   bool changePWMFrequencies = false;
   if(changePWMFrequencies)
   {    
     // change up PWM frequencies for motor PWM pins
     // Timer 3 for outputs 2/3/5
-    TCCR3B &= ~(0x07);
-    TCCR3B |= 0x02;
+    TCCR1B &= ~(0x07);
+    TCCR1B |= 0x02;
     
     // Timer 4 for outputs 9,10  
     TCCR2B &= ~(0x07);
     TCCR2B |= 0x02;
   }
 
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(9, OUTPUT);
-  pinMode(10, OUTPUT);
-
-//  Serial.println("Setup");
-    
-//    analogWrite(2, 200);
-    digitalWrite(2, HIGH);
-    digitalWrite(3, LOW);
-    
-//    analogWrite(8, 200);
-    digitalWrite(9, HIGH);
-    digitalWrite(10, LOW);
 }
 
 bool doneTurn = false;
@@ -55,14 +37,9 @@ void loop()
 {
   const int GOAL_AVG = 170;
 
-//    Serial.println("loop");
   if(currentTest == RunMotors)
   {
-//   .en = 10,
-//  .in1 = 8,
-//  .in2 = 5
     Serial.println("run motors");
-
   }
   else if(currentTest == DriveStraightWIMU)
   {
